a3e584bac4f925176190401f664c8834c2554615
Michael4Camps <C24354791@myTUDublin.ie>
2024-11-14 15:48:06 +0000

    patrol + chase work + death

--- GameJam Project/Assets/Scripts/Michael/ChaseCode.cs
+++ GameJam Project/Assets/Scripts/Michael/ChaseCode.cs
@@ -7,17 +7,14 @@ public class Enemy : MonoBehaviour {
     public bool flip;
     public float speed;
 
-    private void update()
-    {
+    private void Update() {
         Vector3 scale = transform.localScale;
 
-        if (player.transform.position.x > transform.position.x)
-        {
+        if (player.transform.position.x > transform.position.x) {
             scale.x = Mathf.Abs(scale.x) * -1 * (flip ? 1 : -1);
-            transform.Translate(x: speed * Time.deltaTime, y: 0, z: 0);
-        }else
-        {
-            scale.x = mathf.Abs(scale.x) * (flip ? -1 : 1);
+            transform.Translate(x:speed * Time.deltaTime, y: 0, z: 0);
+        } else {
+            scale.x = Mathf.Abs(scale.x) * (flip ? -1 : 1);
             transform.Translate(x:speed * Time.deltaTime * -1, y:0, z: 0);
 
         }
